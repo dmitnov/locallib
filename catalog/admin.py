e036b9df6d4015fb6d58206946074c4c14c1455a
dmitry <dmitry@dmitrys-iMac.local>
2019-12-21 16:19:44 +0300

    Add list BookInstance and list books in Author

--- catalog/admin.py
+++ catalog/admin.py
@@ -10,15 +10,24 @@ admin.site.register(Genre)
 #admin.site.register(BookInstance)
 
 # Define the admin class
+class BookInline(admin.TabularInline):
+    extra = 0
+    model = Book
+
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
-    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    #fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+
 
 # Register the admin class with the associated model
+
 admin.site.register(Author, AuthorAdmin)
 
 # Register the Admin classes for Book using the decorator
 class BooksInstanceInline(admin.TabularInline):
+    extra = 0
     model = BookInstance
 
 @admin.register(Book)
@@ -31,6 +40,7 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance) 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id')
  
     fieldsets = (
         (None, {
