cfcba6bc5e72bda61014aac109f0b72b08d9445b
dmitry <dmitry@dmitrys-iMac.local>
2019-12-25 14:15:15 +0300

    Add author list and detail

--- catalog/views.py
+++ catalog/views.py
@@ -32,6 +32,12 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
 
+class AuthorListView(generic.ListView):
+    model = Author
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+
 # class BookListView(generic.ListView):
     # model = Book
     # paginate_by = 3
